791edb118d729db19225be8dbae11bcf3cc91050
lirik <lightlirikus@yandex.ru>
2023-02-08 23:51:47 +0300

    добавили задачу про гири

---
+++ giri/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 4;
+int c = 9;
+int d = 7;
+int e = 5;
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("Вес самой тяжелой гири: ");
+Console.Write(max);
